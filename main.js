79ac92553d2a309d82608b65215830be9e854f13
OXIDE <oxidiu777@gmail.com>
2023-05-06 13:40:58 +0300

    done

--- main.js
+++ main.js
@@ -12,8 +12,107 @@ function isEven(num) {
     return num % 2 === 0;
 }
 
+
+
+const actionCallback = (arg) => {
+    const parsed = JSON.parse(arg);
+    return parsed
+}
 function convert(data, actionCb) {
     return actionCb(data);
 }
-
 const convertedData = convert(jsonData, actionCallback);
+// console.log(convertedData);
+
+const {data} = convertedData;
+// console.log(data);
+
+class DataProcess {
+    constructor(data) {
+        this._data = data;
+    }
+
+    sortData() {
+        this._data.sort((a, b) => a - b)
+    }
+
+    calculateTotal() {
+        this.total = this._data.reduce((acc, current) => {
+            return acc + current;
+        })
+    }
+    isEven() {
+        return this.total % 2 === 0;
+    }
+
+    doDouble() {
+        this.doubled = this._data.map((num) => num * 2)
+    }
+    
+    static getOdd(data) {
+        return data.filter((num) => num % 2 === 1)
+    }
+
+    static getEven(data) {
+        return data.filter((num) => num % 2 === 0)
+    }
+
+    printProp() {
+        console.log("Data:")
+        console.log(this._data);
+
+        console.log("Total:")
+        console.log(this.total);
+
+        console.log("Doubled:")
+        console.log(this.doubled);
+        
+    }
+
+    smaller() {
+        this.smaller = this._data.filter((num) => num < 50)
+    }
+
+    toJSON() {
+        return {
+            data: this._data,
+            total: this.total,
+            doubled: this.doubled
+        }
+    }
+}
+const dataProcess = new DataProcess(data);
+
+console.log("Unsorted:")
+console.log(dataProcess)
+
+dataProcess.sortData();
+console.log("Sorted:")
+console.log(dataProcess);
+
+dataProcess.calculateTotal()
+console.log("Total:")
+console.log(dataProcess.total)
+
+console.log("Is total even?")
+console.log(dataProcess.isEven())
+
+dataProcess.doDouble();
+console.log("Doubled:")
+console.log(dataProcess.doubled);
+
+console.log("Odd numbers:")
+console.log(DataProcess.getOdd(data));
+
+console.log("Even numbers:")
+console.log(DataProcess.getEven(data));
+
+console.log("Proprieties:")
+dataProcess.printProp()
+
+dataProcess.smaller();
+console.log("Smaller:")
+console.log(dataProcess.smaller)
+
+console.log("JSON Stringified:")
+console.log(JSON.stringify(dataProcess))
